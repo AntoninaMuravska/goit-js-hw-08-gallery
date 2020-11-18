1479495979de105ed86a36d3be351f58c550269c
Antonina Muravska <at.tonya@gmail.com>
2020-11-19 00:43:59 +0200

    add function createGalleryMurkup

--- gallery-items.js
+++ gallery-items.js
@@ -78,3 +78,25 @@ export default [
   </a>
 </li> */
 
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+function createGalleryMurkup(array) {
+  return array.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  }).join('');
+}
+
+console.log(createGalleryMurkup)
